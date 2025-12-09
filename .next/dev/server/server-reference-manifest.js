aaf24f88988876a3538a4317b07e14b2f5c85de2
bashytech007 <bashiraremu474@gmail.com>
2025-12-09 16:21:46 +0100

    Add .next to gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DUpbm6fK0/sQFMFXGJqLVIQehX9fLg/G2xXudsXnIpU=\"\n}"
--- .next/dev/static/chunks/Desktop_craft_craft-connect_app_sign-in_page_5876faaa.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/b1c1a_0910e679._.js",
-  "static/chunks/Desktop_craft_craft-connect_e09c4173._.js"
-],
-    source: "dynamic"
-});
